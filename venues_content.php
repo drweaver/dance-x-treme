3b4d0e40a5e4ca04419b87ea6bdeb72d86e5e2e8
Shane Weaver <sgtw_speed@yahoo.com>
2014-12-24 18:59:51 +0000

    noscript for classes, gallery and venues

--- venues_content.php
+++ venues_content.php
@@ -2,83 +2,38 @@
 <div id="venue-map-loading" class="hidden"><img src="img/spinner.gif"/> Loading...</div>
 <div id="venue-map">
 <noscript>
-<img src="http://maps.googleapis.com/maps/api/staticmap?size=600x600&maptype=roadmap&sensor=false&markers=52.66024,-2.137742|52.556929,-1.935761|52.5807768,-1.9756287|52.564114,-1.804075|52.627305,-1.965877|52.622933,-1.952139|52.607608,-2.155166|52.448359,-2.049567|52.433871,-2.109519|52.440708,-2.074977|52.564125,-1.824436&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><hr>
-<br/><h2 id="/venue/coven">Coven Memorial Hall</h2>
-<h3>Wolverhampton Area</h3>
-<p>Brewood Road, Coven,Wolverhampton, WV9 5DL</p>
-<p><a href="classes.html#/class/coven">Class Timetable</a></p>
-<p><a href="http://maps.google.co.uk/maps?daddr=52.66024,-2.137742&cbll=52.660204,-2.137706&panoid=SaKUJm5GwMHotdvntDzX1w&cbp=12,350.26,,0,5.35&layer=c&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.66024,-2.137742&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
-<br/><h2 id="/venue/greatbarr">Great Barr Memorial Hall</h2>
-<h3>Great Barr Area</h3>
-<p>Corner of Chapel Lane & Birmingham Rd, B43 7BD</p>
-<p><a href="classes.html#/class/greatbarr">Class Timetable</a></p>
-<p><a href="http://maps.google.co.uk/maps?daddr=52.556929,-1.935761&cbll=52.556939,-1.935705&panoid=Nfaov5rS-WW9SprwHYhQwg&cbp=12,322.06,,0,17.04&layer=c&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.556929,-1.935761&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
-<br/><h2 id="/venue/bluecoatacademy">Blue Coat Academy</h2>
-<h3>Walsall Area</h3>
-<p>Birmingham Street, Walsall, West Midlands, WS1 2ND</p>
-<p><a href="classes.html#/class/bluecoatacademy">Class Timetable</a></p>
-<p><a href="http://maps.google.co.uk/maps?daddr=52.5807768,-1.9756287&cbll=52.580741,-1.975667&panoid=pPAfDZH5RBvKXKN7Sp1-8Q&cbp=12,13.73,,0,8.92&layer=c&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.5807768,-1.9756287&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
-<br/><h2 id="/venue/stchads">St Chad's Church Hall</h2>
-<h3>Sutton Coldfield Area</h3>
-<p>Hollyfield Road, Sutton Coldfield, B75 7SN</p>
-<p><a href="classes.html#/class/stchads">Class Timetable</a></p>
-<p><a href="https://maps.google.co.uk/maps?q=B75+7SN&daddr=52.563132,-1.803415&hl=en&ll=52.563132,-1.803415&spn=0.001017,0.005681&sll=52.448356,-2.049567&sspn=0.002262,0.005681&gl=uk&hnear=Sutton+Coldfield+B75+7SN,+United+Kingdom&t=m&z=18&layer=c&cbll=52.563132,-1.803416&panoid=zJxqP22FUpCeKZull63CdA&cbp=11,291.59,,0,-3.48&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.564114,-1.804075&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
-<br/><h2 id="/venue/pelsall">Pelsall Community Centre</h2>
-<h3>Pelsall Area</h3>
-<p>Station Road, Pelsall, WS3 4BQ</p>
-<p><a href="http://maps.google.co.uk/maps?daddr=52.627305,-1.965877&cbll=52.627207,-1.965807&panoid=SzU8h26nq6--wUWy0NBCRg&cbp=12,64.14,,0,6.6&layer=c&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.627305,-1.965877&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
-<br/><h2 id="/venue/shelfield">Shelfield Community Acadamy Dance Studio</h2>
-<h3>Pelsall Area</h3>
-<p>Broad Way, Pelsall, WS4 1BW</p>
-<p><a href="classes.html#/class/shelfield">Class Timetable</a></p>
-<p><a href="http://maps.google.co.uk/maps?daddr=52.622933,-1.952139&cbll=52.622829,-1.95204&panoid=SYS1_v_1rT2GQMBmoOl0yA&cbp=12,50.34,,0,12.9&layer=c&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.622933,-1.952139&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
-<br/><h2 id="/venue/claregate">Claregate Primary School</h2>
-<h3>Wolverhampton Area</h3>
-<p>Chester Avenue, Wolverhampton, WV6 9JU</p>
-<p><a href="classes.html#/class/claregate">Class Timetable</a></p>
-<p><a href="http://maps.google.co.uk/maps?q=WV6+9JU&ie=UTF8&hq=&hnear=Wolverhampton,+West+Midlands+WV6+9JU,+United+Kingdom&gl=uk&daddr=52.607608,-2.155166&ll=52.607608,-2.155166&spn=0.008196,0.016608&z=16&layer=c&cbll=52.607692,-2.155201&panoid=HNMvVtO0rR0bV_vVb7FYdg&cbp=12,135.58,,0,7.56&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.607608,-2.155166&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
-<br/><h2 id="/venue/cornbowhall">Cornbow Hall</h2>
-<h3>Halesowen Area</h3>
-<p>10 Hagley Street, Town Centre, Halesowen, B63 3AF</p>
-<p><a href="https://maps.google.co.uk/maps?q=B63+3AF&hl=en&daddr=52.448359,-2.049567&ll=52.448359,-2.049567&spn=0.001244,0.004807&sll=52.433871,-2.109519&sspn=0.003899,0.009613&gl=uk&hnear=B63+3AF,+United+Kingdom&z=18&layer=c&cbll=52.448356,-2.049565&panoid=H0zsEBGe7Erxf1vN0QRJhw&cbp=11,256.68,,0,1.1&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.448359,-2.049567&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
-<br/><h2 id="/venue/halesowenrugbyclub">Old Halesonians Rugby Club</h2>
-<h3>Stourbridge Area</h3>
-<p>Wassell Grove Lane, Hagley, Stourbridge, DY9 9JP</p>
-<p><a href="classes.html#/class/halesowenrugbyclub">Class Timetable</a></p>
-<p><a href="https://maps.google.co.uk/maps?q=rugby+club+near+Halesowen&hl=en&daddr=52.433871,-2.109519&ll=52.433871,-2.109519&spn=0.003872,0.009613&sll=52.560356,-1.820474&sspn=0.002749,0.004807&oq=halesowen+rugby+club&gl=uk&hq=rugby+club&hnear=Halesowen,+West+Midlands,+United+Kingdom&fll=52.433154,-2.10651&fspn=0.003872,0.009613&z=17&layer=c&cbll=52.433871,-2.109519&panoid=gTj3ktwqh_2rCJynlHqnGQ&cbp=12,116.42,,0,1.38&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.433871,-2.109519&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
-<br/><h2 id="/venue/lutley">Lutley Community Centre</h2>
-<h3>Halesowen Area</h3>
-<p>Brookwillow Road, Halesowen, B63 1BU</p>
-<p><a href="classes.html#/class/lutley">Class Timetable</a></p>
-<p><a href="https://maps.google.co.uk/maps?q=lutley+community+centre&hl=en&daddr=52.440708,-2.074977&ll=52.440708,-2.074977&spn=0.005036,0.011362&sll=52.440709,-2.074978&sspn=0.003579,0.005681&hq=lutley+community+centre&z=17&layer=c&cbll=52.440708,-2.074977&panoid=uEOPoD2wC27JK-3K57ph0g&cbp=12,265.67,,0,3.65&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.440708,-2.074977&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
-<br/><h2 id="/venue/unitedreformedchurch">United Reformed Church (previously Held at Fellowship Hall)</h2>
-<h3>Sutton Coldfield Area</h3>
-<p>1 Brassington Avenue, Sutton Coldfield, B73 6AA</p>
-<p><a href="classes.html#/class/unitedreformedchurch">Class Timetable</a></p>
-<p><a href="https://maps.google.co.uk/maps?q=52.564125,-1.824436&hl=en&ll=52.564125,-1.824436&spn=0.002824,0.005595&sll=52.563825,-1.824317&layer=c&cbp=13,166.44,,0,0&cbll=52.564125,-1.824436&gl=uk&t=m&z=18&iwloc=A&panoid=N6PlwXozBHBrprbdcgfDsA&output=classic" target="_blank">Get Directions</a></p>
-<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers=52.564125,-1.824436&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I">
-<br/><br/><hr/>
+<?php
+$venues_data = json_decode( file_get_contents('data/dance_venues.txt'), true );
 
+$key_param='&key=AIzaSyAzo-Q6qXNjbhaTknSH9K7lsZnlgAkhV3I';
+
+$markers = '';
+$sep = '';
+foreach ($venues_data as $g) {
+	if( $g['enabled'] == FALSE )
+		continue;
+	$p = $g['position'];
+	$markers = $markers . $sep . $p['lat'].','.$p['lng'];
+	$sep = '|';
+}
+print('<img src="http://maps.googleapis.com/maps/api/staticmap?size=600x600&maptype=roadmap&sensor=false&markers='.$markers.$key_param.'">'); 
+
+print('<br/><hr>');
+
+foreach ($venues_data as $g) {
+	if( $g['enabled'] == FALSE )
+		continue;
+	print('<br/><h2 id="/venue/'.$g['id'].'">'.$g['name'].'</h2>');
+	print('<h3>'.$g['area'].' Area</h3>');
+	print('<p>'.$g['address'].'</p>');
+	if( array_key_exists('timetable', $g) )
+		print('<p><a href="classes#/class/'.$g['id'].'">Class Timetable</a></p>');
+	print('<p><a href="'.$g['svUrl'].'" target="_blank">Get Directions</a></p>');
+	$p = $g['position'];
+	print('<img src="http://maps.googleapis.com/maps/api/staticmap?zoom=15&size=600x200&maptype=roadmap&sensor=false&markers='.$p['lat'].','.$p['lng'].$key_param.'">');
+	print('<br/><br/><hr/>');
+}
+?>
 </noscript>
 </div>
+
