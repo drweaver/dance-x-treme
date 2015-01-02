bd681364d6ed3c33f48f083afd515a53fed4dff2
Shane Weaver <sgtw_speed@yahoo.com>
2015-01-02 17:35:25 +0000

    404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+
+include('./default_vars.php');
+
+$t_description = 'Page not found!';
+$t_content_inc = './404_content.php';
+
+include('./master.php');
+
+?>
---
+++ 404_content.php
@@ -0,0 +1,12 @@
+<div class="container">
+	
+<div class="page-header">
+    <h1>Oops!</h1>
+    </div>
+
+	<div class="jumbotron">
+    
+<p>The page you were looking for just missed the last waltz. <a class="btn btn-lg btn-primary" href="/" role="button">Try our homepage</a>
+</p>
+</div>
+</div>
