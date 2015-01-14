f98b1160138d2c45f37df1252ddfdae2d25bf1f4
Shane Weaver <sgtw_speed@yahoo.com>
2015-01-14 17:39:58 +0000

    indentation

--- master.php
+++ master.php
@@ -110,18 +110,18 @@
     
     </script>
     <script>
-    $(document).ready(function() {
-        $('#<?php echo $t_navname;?>').addClass('active');
-    });
-  </script>
-  <div id="fb-root"></div>
-  <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=178227298914590&version=v2.0";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));</script>
+      $(document).ready(function() {
+          $('#<?php echo $t_navname;?>').addClass('active');
+      });
+    </script>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=178227298914590&version=v2.0";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
   </body>
 </html>
 
