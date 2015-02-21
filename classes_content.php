8b42db5ef192eb8402fa843957ba78a27e28d1ef
Shane Weaver <sgtw_speed@yahoo.com>
2015-02-21 15:17:07 +0000

    better layout for class FAQs

--- classes_content.php
+++ classes_content.php
@@ -4,25 +4,33 @@
 <div class="page-header">
     <h1>Classes</h1>
 </div>
-<p>Dance classes for adults of all ages and abilities, couples and singles. We teach Ballroom and Latin with a great selection of music. 
-Our classes are held at safe, superb facilities at all our <a href="/venues">venues</a>. <a href="/contact">Private lessons</a> are also available.</p>
 
-<dl class="dl-horizontal">
-  <dt>Do I need to book?</dt>
-  <dd>No, just turn up at a class at the appropriate time</dd>
-  <dt>Is there a booking fee?</dt>
-  <dd>No</dd>
-  <dt>How do I pay?</dt>
-  <dd>Just pay for individual classes as and when you attend</dd>
-  <dt>Can I attend multiple?</dt>
-  <dd>Yes, absolutely</dd>
-  <dt>What shall I wear?</dt>
-  <dd>Comfortable shoes and casual clothing</dd>
-  <dt>Do you teach children?</dt>
-  <dd>No, Adults only</dd>
-  <dt>I have more questions</dt>
-  <dd>Don't hestitate to <a href="/contact">contact us</a></dd>
-</dl>
+<div class="row">
+	<div class="col-lg-6 col-md-4">
+		<p>Dance classes for adults of all ages and abilities, couples and singles. We teach Ballroom and Latin with a great selection of music. 
+		Our classes are held at safe, superb facilities at all our <a href="/venues">venues</a>. <a href="/contact">Private lessons</a> are also available.</p>
+	</div>	
+
+	<div class="col-lg-6 col-md-8">
+	
+		<dl class="dl-horizontal">
+		  <dt>Do I need to book?</dt>
+		  <dd>No, just turn up at a class at the appropriate time</dd>
+		  <dt>Is there a booking fee?</dt>
+		  <dd>No</dd>
+		  <dt>How do I pay?</dt>
+		  <dd>Just pay for individual classes as and when you attend</dd>
+		  <dt>Can I attend multiple?</dt>
+		  <dd>Yes, absolutely</dd>
+		  <dt>What shall I wear?</dt>
+		  <dd>Comfortable shoes and casual clothing</dd>
+		  <dt>Do you teach children?</dt>
+		  <dd>No, Adults only</dd>
+		  <dt>I have more questions</dt>
+		  <dd>Don't hestitate to <a href="/contact">contact us</a></dd>
+		</dl>
+	</div>
+</div>
 
 <hr/>
 	<div ng-controller="ClassController">
