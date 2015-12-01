7c7e58c305245e095e603742ecd8f2aa80340cb5
Shane Weaver <drweaver@users.noreply.github.com>
2015-12-01 12:13:33 +0000

    change price of private lessons

--- contact_content.php
+++ contact_content.php
@@ -17,7 +17,7 @@
     
 <p><strong>Private lessons</strong> are available on Sunday's at <a href="venues#/venue/lutley">Lutley community centre</a>, Brookwillow Road, Halesowen via 
     school entrance. 
-Lessons cost &pound;20 for 1 hour and are pre-bookable by seeing Sue at class or using the 
+Lessons cost &pound;22 for 1 hour and are pre-bookable by seeing Sue at class or using the 
 contact details above.
 </p>
 </div>
