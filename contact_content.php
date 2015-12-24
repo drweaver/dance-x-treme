29f139d87baf84eee6b45a3b2774bdcb9e4dd9e8
Shane Weaver <sgtw_speed@yahoo.com>
2015-12-24 17:14:46 +0000

    fix to new hashbang to link on contact page

--- contact_content.php
+++ contact_content.php
@@ -15,7 +15,7 @@
 
 	<div class="jumbotron">
     
-<p><strong>Private lessons</strong> are available on Sunday's at <a href="venues#/venue/lutley">Lutley community centre</a>, Brookwillow Road, Halesowen via 
+<p><strong>Private lessons</strong> are available on Sunday's at <a href="/venues#!/by-venue/at-lutley-community-centre">Lutley community centre</a>, Brookwillow Road, Halesowen via 
     school entrance. 
 Lessons cost &pound;22 for 1 hour and are pre-bookable by seeing Sue at class or using the 
 contact details above.
