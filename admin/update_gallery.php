036d47c04a1109ce9f71fce12f31c5bf1357304b
drweaver <sgtw_speed@yahoo.com>
2017-01-30 22:44:49 +0000

    make backup

--- admin/update_gallery.php
+++ admin/update_gallery.php
@@ -4,7 +4,8 @@
 
 <?php 
 $json_string = json_encode(json_decode($HTTP_RAW_POST_DATA), JSON_PRETTY_PRINT);
-file_put_contents('dance_galleries.txt', $json_string); 
+file_put_contents('dance_galleries.txt', $json_string);
+exec( 'cp ./dance_galleries.txt ./bak/dance_galleries.txt.$(date +%Y%m%d_%H%M%S)');
 exec( './publish_gallery.sh >> ./publish_gallery.log');
 ?>
 
--- admin/update_venue.php
+++ admin/update_venue.php
@@ -4,7 +4,8 @@
 
 <?php 
 $json_string = json_encode(json_decode($HTTP_RAW_POST_DATA), JSON_PRETTY_PRINT);
-file_put_contents('dance_venues.txt', $json_string); 
+file_put_contents('dance_venues.txt', $json_string);
+exec( 'cp ./dance_venues.txt ./bak/dance_venues.txt.$(date +%Y%m%d_%H%M%S)');
 exec( './publish_venue.sh >> ./publish_venue.log');
 ?>
 
