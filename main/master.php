ea659312bacc48c67bfa77e66c86152da3c859f8
drweaver <shane@Shanes-MacBook-Pro.local>
2017-12-24 20:03:34 +0000

    fix caching

--- main/master.php
+++ main/master.php
@@ -98,7 +98,7 @@
     <!--
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
     -->
-    <script src="js/dancextreme.js"></script>
+    <script src="js/dancextreme.js?_=20171224"></script>
     <script type="text/javascript">
       var _gaq = _gaq || [];
       _gaq.push(['_setAccount', 'UA-12475745-1']);
