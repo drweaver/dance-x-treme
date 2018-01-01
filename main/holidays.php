7a336b02e78891b9712596c3e801488862d2bcb7
drweaver <shane@Shanes-MacBook-Pro.local>
2018-01-01 19:49:40 +0000

    Holiday page added

---
+++ main/holidays.php
@@ -0,0 +1,12 @@
+<?php
+
+include('./default_vars.php');
+
+$t_title_append = ' - Holidays';
+$t_description = 'Dance holidays and dance weekends';
+$t_content_inc = './holidays_content.php';
+$t_navname = 'nav-holidays';
+
+include('./master.php');
+
+?>
---
+++ main/holidays_content.php
@@ -0,0 +1,79 @@
+<div class="container">
+	
+    <div class="page-header">
+        <h1>Dance Holidays</h1>
+    </div>
+
+    <p>We organise several dance holidays throughout the year. 
+    The itinerary generally starts with a social dance on Friday evening to get you warmed up. 
+    Saturday continues the dancing with either a workshop or a trip to 
+    Blackpool Tower Ballroom followed by a themed or formal dance in the evening.</p>
+    <p>Booking forms and itinerary are available in class.<p>
+    <p><strong>These holidays have proved very popular, 
+    however there is limited availability at the hotel so please book early to avoid disappointment!</strong></p>
+
+    <p>Want to take a glimpse at previous dance weekends? Check out our <a href="/gallery#!/weekend">Gallery</a></p>
+
+
+    <br/>
+	<div class="panel panel-primary">
+        <div class="panel-heading">
+            <h3 class="panel-title">Rock 'n' Roll Dance Weekend 2018</h3>
+        </div>
+        <div class="panel-body">
+
+            <h3><span class="label label-default"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+            11th May 2018</span></h3>
+            <h3><span class="label label-default"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+            22nd June 2018</span></h3>
+            <br/>
+            <p>A two night Rock 'n' Roll break staying at the Wessex Hotel, Street, Somerset.</p>
+            <p>Includes:<br/>
+            <ul>
+                <li>2 Nights Bed, Breakfast &amp; Evening Meals</li>
+                <li>Friday Evening Social Dance</li>
+                <li>Saturday Morning Rock 'n' Roll Dance Workshop</li>
+                <li>Saturday Evening Rock 'n' Roll Themed Social Dance</li>
+            </ul>
+            </p>
+
+            <h3><span class="label label-default">
+                <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 
+                £160 per person</span></h3>
+        </div>
+    </div>
+    <br/>
+	<div class="panel panel-primary">
+        <div class="panel-heading">
+            <h2 class="panel-title">Dancing at the Tower 2018</h2>
+        </div>
+        <div class="panel-body">
+            <h3><span class="label label-default"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> 
+            28th September 2018</span></h3>
+            <h3><span class="label label-default"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> 
+            19th October 2018</span></h3>
+            <br/>
+            <p>A two night break staying at the stylish Inn on the Prom, St Annes on Sea, Nr Blackpool!
+            This hotel boasts an enviable sea-front location and guests are welcome 
+            to use the health club and spa facilities located inside the hotel.</p>
+
+            <p>
+            Includes:
+            <ul>
+                <li>2 Nights Bed, Breakfast &amp; Evening Meals</li>
+                <li>Friday Evening Social Dance</li>
+                <li>Saturday Morning Transport to Blackpool Tower for Lunch and <strong>Dancing at Blackpool's Tower Ballroom</strong></li>
+                <li>Saturday Evening Champagne Reception &amp; Formal Dinner / Dance</li>
+                <li>Plus, use of Hotel's Leisure Facilities</li>
+                <li>Entrance Ticket will be provided for Blackpool Tower Ballroom</li>
+            </ul>
+            </p>
+
+            <h3><span class="label label-default">
+                <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> 
+                £230 per person</span></h3>
+        </div>
+
+    </div>
+
+</div>
