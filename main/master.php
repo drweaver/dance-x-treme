5e8a0546296b3e84b7435f91e495f0fa0c72a9cb
drweaver <shane@Shanes-MacBook-Pro.local>
2018-01-27 17:18:18 +0000

    remove cookie consent

--- main/master.php
+++ main/master.php
@@ -16,13 +16,6 @@
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 
-    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
-    <script type="text/javascript">
-        window.cookieconsent_options = {"message":"We use cookies to provide social media features and to analyse our traffic.","dismiss":"Got it!","learnMore":"See details","link":"https://support.google.com/analytics/answer/2700409?hl=en","theme":"dark-bottom"};
-    </script>
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
-    <!-- End Cookie Consent plugin -->
-
     <!-- Custom styles for this template -->
     <style> <?php include('./css/dancextreme.css');?> </style>
     <!--
